51856c399200548aefe233b262a89519691bb3b4
shrsaurav002 <67121894+shrsaurav002@users.noreply.github.com>
2020-09-29 10:23:37 -0400

    data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessComponent } from './success/success.component';
+import { WarningComponent } from './warning/warning.component';
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
+    ServersComponent,
+    SuccessComponent,
+    WarningComponent
   ],
   imports: [
     BrowserModule,
